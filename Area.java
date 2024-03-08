214556e7e59d870d58763e6f2ee6bffda4a5329e
deepakkumr09 <sahudkkumar9598@gmail.com>
2024-03-08 20:07:42 +0530

    clone karne ke liye changes

--- Area.java
+++ Area.java
@@ -12,3 +12,82 @@ public class Area {
         // int b = (int)a;
         // System.out.println(b);
        
+import java.util.*;
+public class Area {
+    public static void main(String[] args) {
+        // System.out.println("Enter the circle radius :");
+        // float rad;
+        // Scanner obj = new Scanner(System.in);
+        // rad = obj.nextFloat();
+        // float area = 3.14f * rad * rad;
+        // System.out.println(area);
+        System.out.println("Type Casting .........................");
+        // float a =21474472.5675f ;
+        // int b = (int)a;
+        // System.out.println(b);
+       import java.util.*;
+public class Area {
+    public static void main(String[] args) {
+        // System.out.println("Enter the circle radius :");
+        // float rad;
+        // Scanner obj = new Scanner(System.in);
+        // rad = obj.nextFloat();
+        // float area = 3.14f * rad * rad;
+        // System.out.println(area);
+        System.out.println("Type Casting .........................");
+        // float a =21474472.5675f ;
+        // int b = (int)a;
+        // System.out.println(b);
+       import java.util.*;
+public class Area {
+    public static void main(String[] args) {
+        // System.out.println("Enter the circle radius :");
+        // float rad;
+        // Scanner obj = new Scanner(System.in);
+        // rad = obj.nextFloat();
+        // float area = 3.14f * rad * rad;
+        // System.out.println(area);
+        System.out.println("Type Casting .........................");
+        // float a =21474472.5675f ;
+        // int b = (int)a;
+        // System.out.println(b);
+       vvimport java.util.*;
+public class Area {
+    public static void main(String[] args) {
+        // System.out.println("Enter the circle radius :");
+        // float rad;
+        // Scanner obj = new Scanner(System.in);
+        // rad = obj.nextFloat();
+        // float area = 3.14f * rad * rad;
+        // System.out.println(area);
+        System.out.println("Type Casting .........................");
+        // float a =21474472.5675f ;
+        // int b = (int)a;
+        // System.out.println(b);
+       import java.util.*;
+public class Area {
+    public static void main(String[] args) {
+        // System.out.println("Enter the circle radius :");
+        // float rad;
+        // Scanner obj = new Scanner(System.in);
+        // rad = obj.nextFloat();
+        // float area = 3.14f * rad * rad;
+        // System.out.println(area);
+        System.out.println("Type Casting .........................");
+        // float a =21474472.5675f ;
+        // int b = (int)a;
+        // System.out.println(b);
+       import java.util.*;
+public class Area {
+    public static void main(String[] args) {
+        // System.out.println("Enter the circle radius :");
+        // float rad;
+        // Scanner obj = new Scanner(System.in);
+        // rad = obj.nextFloat();
+        // float area = 3.14f * rad * rad;
+        // System.out.println(area);
+        System.out.println("Type Casting .........................");
+        // float a =21474472.5675f ;
+        // int b = (int)a;
+        // System.out.println(b);
+       
